ec480312d7eb7e7f9d0c7f8317ea1cc0a7585437
carnageX <chasebenn@gmail.com>
2015-05-27 23:52:40 -0500

    +Added EncodingType class

---
+++ ChecksumVerifier/EncodingType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChecksumVerifier
+{
+    class EncodingType
+    {
+        public string Name { get; set; }
+        public Encoding Type { get; set; }
+
+        public EncodingType(string name, Encoding type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
